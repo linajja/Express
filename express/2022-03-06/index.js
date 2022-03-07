7da5e3f6e9a1352b30c21d6ac307f8fed4bb736f
linajja <lina@netmarkuab.com>
2022-03-07 16:41:49 +0200

    express

--- express/2022-03-06/index.js
+++ express/2022-03-06/index.js
@@ -17,44 +17,52 @@ app.get('/admin', function (req, res) {
   return
 })
 
+// app.get('/', function (req, res) {
+//   if (Object.keys(req.query).length > 0) {
+//     res.render('login', {
+//       pirma: req.query.vardas,
+//       antra: req.query.pavarde,
+//       trecia: req.query.adresas,
+//       ketvirta: req.query.telefonas,
+//       penkta: req.query.email
+//     })
+//     return
+//   }
+//   res.sendFile(__dirname + '/templates/index.html')
+// })
+
 app.get('/', function (req, res) {
-  if (req.query.login === "admin@inv.lt" &&
+  if (req.query.login != '' &&
+    req.query.password != '' &&
+    req.query.login === "admin@inv.lt" &&
     req.query.password === "1234") {
-    console.log(req.query.login)
     res.redirect('http://localhost:3000/administratorius')
     return
-  }
-  if (Object.keys(req.query).length > 0) {
-    console.log(req.query)
+  } else if (req.query.login != '' &&
+    req.query.password != '' &&
+    req.query.login != "admin@inv.lt" &&
+    req.query.password != "1234") {
+    let info = "Neteisingas el. pašto adresas arba slaptažodis"
+    res.render('person', { info })
+    return
+  } else {
     res.redirect('http://localhost:3000')
     return
   }
+})
 
-  res.sendFile(__dirname + '/templates/index.html')
+app.get('/:vardas', function (req, res) {
+  let pirma = req.params.vardas
+  res.send('<h1>' + pirma + '</h1>') //Nurodome grazinama turini atgal i narsykle
+})
 
-  // app.get('/:vardas/:pavarde/:adresas/:telefonas/:elpastas', function (req, res) {
-  //   res.render('persons', {
-  //     vardas: req.query.vardas,
-  //     pavarde: req.query.pavarde,
-  //     adresas: req.query.adresas,
-  //     telefonas: req.query.telefonas,
-  //     elpastas: req.query.elpastas
-  //   })
-  // })
-
-  app.get('/:vardas', function (req, res) {
-    let vardas = req.params.vardas
-    res.send('<h1>' + vardas + '</h1>') //Nurodome grazinama turini atgal i narsykle
-  })
-
-  app.get('/:vardas/:pavarde/:adresas/:telefonas/:elpastas', function (req, res) {
-    let vardas = req.params.vardas
-    let pavarde = req.params.pavarde
-    let adresas = req.params.adresas
-    let telefonas = req.params.telefonas
-    let elpastas = req.params.elpastas
-    res.send('<h1>' + vardas + pavarde + adresas + telefonas + elpastas + '</h1>') //Nurodome grazinama turini atgal i narsykle
-  })
+app.get('/:pirma/:antra/:trecia/:ketvirta/:penkta', function (req, res) {
+  let pirma = req.params.pirma
+  let antra = req.params.antra
+  let trecia = req.params.trecia
+  let ketvirta = req.params.ketvirta
+  let penkta = req.params.penkta
+  res.render('login', { pirma, antra, trecia, ketvirta, penkta }) //Nurodome grazinama turini atgal i narsykle
 })
 
 app.listen(3000) //Nurodomas portas ir inicijuojamas serveris
